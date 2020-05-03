665c0abd9a05d022ff3375ee20b09391a333a572
Indrajith C <indranspeaks@gmail.com>
2020-05-03 15:52:18 +0530

    dyamic color change success

--- assets/js/popup.js
+++ assets/js/popup.js
@@ -4,6 +4,12 @@ $(function () {
   $("#color-component").submit(function (event) {
     event.preventDefault();
     const color = colorPicker.val();
-    alert(color);
+
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+      chrome.tabs.sendMessage(tabs[0].id, {
+        todo: "changeColor",
+        clickedColor: color,
+      });
+    });
   });
 });
--- content.js
+++ content.js
@@ -1,1 +1,8 @@
 chrome.runtime.sendMessage({ todo: "showPageAction" });
+
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+  if (request.todo === "changeColor") {
+    const adColor = `#${request.clickedColor}`;
+    $(".api h1").attr("style", `color: ${adColor} !important;`);
+  }
+});
